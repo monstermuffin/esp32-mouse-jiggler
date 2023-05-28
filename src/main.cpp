bd1298798d0aa69bb2b5d41373f520888becacde
Christian <christian@serverless.industries>
2023-05-29 00:02:52 +0200

    longpress buttons and multiple bluetooth mac addresses

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #define JIGGLE_MAX_DISTANCE 20
 #define INTERVAL_LIST { 30, 90, 180, 300, 600, 900 }
 #define DEFAULT_INTERVAL 2
+#define NUM_CHANNELS 3
 
 // Display Configs
 #define SCREEN_WIDTH 128
@@ -24,6 +25,19 @@
 #define BUTTON_UP 5
 #define BUTTON_DOWN 27
 #define DEBOUNCE_DELAY 250
+#define LONG_PRESS 1000
+
+// Types
+#define BUTTON_NONE 0
+#define BUTTON_PRESS 1
+#define BUTTON_LONGPRESS 2
+
+struct ButtonState {
+    bool init;
+    u_int64_t pressed;
+    u_int64_t longpress;
+    u_int64_t released;
+};
 
 // Initialize Bluetooth
 BleMouse bleMouse("Bluetooth Mouse 4.2", "Important Technologies", 42);
@@ -55,22 +69,62 @@ void moveMouse()
     }
 }
 
-bool buttonState(int pin, unsigned long now, unsigned long *lastBounce)
+short buttonState(int pin, unsigned long now, ButtonState *buttonState)
 {
-    if ((now - *lastBounce) > DEBOUNCE_DELAY && digitalRead(pin) == LOW)
+    if (buttonState->init == false && digitalRead(pin) == HIGH)
     {
-        *lastBounce = now;
-        return true;
+        // first unpressed state registered
+        buttonState->init = true;
+    }
+    else if(buttonState->init == false)
+    {
+        // button was pressed before firmware started, ignore
+        return BUTTON_NONE;
     }
 
-    return false;
+    if (buttonState->pressed == 0 && digitalRead(pin) == LOW)
+    {
+        // button pressed
+        buttonState->pressed = millis();
+    }
+    else if (buttonState->pressed > 0 && buttonState->released == 0 && buttonState->longpress == 0 && digitalRead(pin) == LOW)
+    {
+        // button still pressed
+        if (millis() - buttonState->pressed > LONG_PRESS)
+        {
+            // it was a long press
+            buttonState->longpress = millis();
+            return BUTTON_LONGPRESS;
+        }
+    }
+    else if (buttonState->pressed > 0 && buttonState->released == 0 && digitalRead(pin) == HIGH)
+    {
+        // button was released
+        buttonState->released = millis();
+        if (buttonState->longpress == 0)
+        {
+            // no longpress recorded, so it's a short press
+            return BUTTON_PRESS;
+        }
+    }
+    else if (buttonState->pressed > 0 && buttonState->released > 0 && millis() - buttonState->released > DEBOUNCE_DELAY)
+    {
+        // button released, debounce time expired
+        buttonState->pressed = 0;
+        buttonState->released = 0;
+        buttonState->longpress = 0;
+    }
+
+    return BUTTON_NONE;
 }
 
 // --> Logic
 
+unsigned short bluetoothChannelOffset = 0;
 unsigned long now = 0;
-unsigned long lastDebounceTimeUp = 0;
-unsigned long lastDebounceTimeDown = 0;
+ButtonState buttonStateTop;
+ButtonState buttonStateBottom;
+short buttonResult;
 unsigned long lastJiggle;
 unsigned long lastDisplayUpdate = 0;
 bool running = true;
@@ -85,6 +139,7 @@ int jiggle_interval;
 char animation[] = { '-', '\\', '|', '/' };
 size_t numAnimations = sizeof(animation) / sizeof(animation[0]);
 int8_t i_animation = 0;
+char s [22];
 
 void setup()
 {
@@ -97,6 +152,14 @@ void setup()
     preferences.begin("app", false);
     current_interval = preferences.getShort("intv", DEFAULT_INTERVAL);
     jiggle_interval = intervals[current_interval] * 1000;
+
+    // mac address
+    // https://generate.plus/en/address/mac
+    // Logitech Inc
+    bluetoothChannelOffset = preferences.getUShort("macoffset", 0);
+    uint8_t macoffset = 0xAE + bluetoothChannelOffset;
+    uint8_t new_mac[8] = { 0xEC, 0x81, 0x93, 0x37, macoffset, 0xCB };
+    esp_base_mac_addr_set(new_mac);
 
     // Button pins
     pinMode(BUTTON_UP, INPUT_PULLUP);
@@ -129,22 +192,36 @@ void loop()
         dirty = true;
     }
 
-    if (buttonState(BUTTON_UP, now, &lastDebounceTimeUp))
+    buttonResult = buttonState(BUTTON_UP, now, &buttonStateTop);
+    if (buttonResult == BUTTON_PRESS)
     {
-        Serial.println("pressed_up");
+        Serial.println("shortpressed_top");
         running = !running;
         dirty = true;
         lastJiggle = now;
     }
-
-    if (buttonState(BUTTON_DOWN, now, &lastDebounceTimeDown))
+    else if (buttonResult == BUTTON_LONGPRESS)
     {
-        Serial.println("pressed_down");
+        Serial.println("longpressed_top");
+    }
+
+    buttonResult = buttonState(BUTTON_DOWN, now, &buttonStateBottom);
+    if (buttonResult == BUTTON_PRESS)
+    {
+        Serial.println("shortpressed_bottom");
         dirty = true;
         current_interval = (current_interval + 1) % numIntervals;
         jiggle_interval = intervals[current_interval] * 1000;
         lastJiggle = now;
         preferences.putShort("intv", current_interval);
+    }
+    else if (buttonResult == BUTTON_LONGPRESS)
+    {
+        Serial.println("longpressed_bottom");
+        preferences.putUShort("macoffset", (preferences.getUShort("macoffset", 0) + 1) % NUM_CHANNELS);
+
+        // TODO: solve issues with restarting BleMouse
+        ESP.restart();
     }
 
     newConnectState = bleMouse.isConnected();
@@ -176,14 +253,16 @@ void loop()
             display.print(animation[i_animation]);
 
             display.setCursor(2, 11);
-            char s [22];
             sprintf (s, "Next in %ds", nextJiggleDiff / 1000);
             display.print(F(s));
         }
 
         display.setCursor(2, 22);
-        char s [22];
-        sprintf (s, "Interval: %ds", intervals[current_interval]);
+        sprintf (s, "Intv: %ds", intervals[current_interval]);
+        display.print(F(s));
+
+        display.setCursor(96, 22);
+        sprintf (s, "Ch: %d", bluetoothChannelOffset);
         display.print(F(s));
 
         display.display();
